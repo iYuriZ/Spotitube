3bd94998688aed1b6f85721814ddae6a0ef974db
iYuriZ <yuriz@live.nl>
2019-05-01 11:34:41 +0200

    settings deployment

--- src/main/java/me/yuri/oosedea/rest/LoginController.java
+++ src/main/java/me/yuri/oosedea/rest/LoginController.java
@@ -1,4 +1,26 @@
 package me.yuri.oosedea.rest;
 
+import me.yuri.oosedea.service.LoginService;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/login")
 public class LoginController {
+
+    @Inject
+    LoginService loginService;
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response login(String user, String password) {
+        return Response.ok().build();
+    }
+
 }
---
+++ src/main/java/me/yuri/oosedea/service/LoginService.java
@@ -0,0 +1,6 @@
+package me.yuri.oosedea.service;
+
+public class LoginService {
+
+
+}
